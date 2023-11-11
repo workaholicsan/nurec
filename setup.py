403140670e772804fdc13c7a38c632f2a063c781
Alexander Polyakov <backstabe@gmail.com>
2023-11-11 22:20:54 +0400

    repack directory

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
   name='nurec',
   version='0.1',
+  license='MIT',
+  summary='Сhecking data recognition',
   packages=find_packages(),
   install_requires=[line.strip() for line in open("requirements.txt").readlines()],
 )
