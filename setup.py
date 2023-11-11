6f337b1b8fa4d4ac408ff2aad10020445cb4eb7b
Alexander Polyakov <backstabe@gmail.com>
2023-11-11 22:41:20 +0400

    fix: fixtures upload

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import setup, find_packages
 setup(
   name='nurec',
   version='0.1',
-  license='MIT',
-  summary='Сhecking data recognition',
+  description='Checking data recognition',
+  author='Aleksandr Poliakov',
+  author_email='backstabe@gmail.com',
   packages=find_packages(),
   install_requires=[line.strip() for line in open("requirements.txt").readlines()],
+  package_data={
+    'nurec': ['verification/*', 'images/*']
+  }
 )
